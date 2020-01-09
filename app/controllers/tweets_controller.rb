347d4c5bc95cdea5ea8303c24bf1adda5069d05b
Sophie Gu <sophieqgu@gmail.com>
2020-01-09 04:50:38 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -57,7 +57,7 @@ class TweetsController < ApplicationController
   patch '/tweets/:id' do 
     @tweet = Tweet.find(params[:id])
     if !params[:content] == ""
-      @tweet.update(params)
+      @tweet.create(params)
       flash[:message] = "Tweet successfully updated."
       redirect to "/tweets/#{@tweet.id}"
     else 
